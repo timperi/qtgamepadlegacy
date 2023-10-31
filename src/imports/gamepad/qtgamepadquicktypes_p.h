0a6eac17a88b947dc5c33d163c941102e3e60906
Arno Rehn <arno@arnorehn.de>
2023-10-31 21:53:55 +0100

    Add missing license header

--- src/imports/gamepad/qtgamepadquicktypes_p.h
+++ src/imports/gamepad/qtgamepadquicktypes_p.h
@@ -1,5 +1,44 @@
+/****************************************************************************
+**
+** Copyright (C) 2023 Arno Rehn <arno@arnorehn.de>.
+**
+** This file is part of the Qt Gamepad Legacy module
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef QTGAMEPADQUICKTYPES_P_H
 #define QTGAMEPADQUICKTYPES_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
 
 #include <QtQml/qqml.h>
 #include <QtGamepadLegacy/QtGamepadLegacy>
